f88a847a66ba9da9ad434243d99fa04ef40753c1
Cualone <june568@naver.com>
2023-05-03 17:15:56 +0900

    3w

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/entity/LikeablePerson.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @Entity
@@ -40,7 +41,11 @@ public class LikeablePerson extends BaseEntity {
 
     // 초 단위에서 올림 해주세요.
     public String getModifyUnlockDateRemainStrHuman() {
-        return "2시간 16분";
+        Duration duration = Duration.between(LocalDateTime.now(), modifyUnlockDate);
+        long remainingHours = duration.toHours();
+        long remainingMinutes = duration.toMinutes() % 60;
+
+        return String.format("%02d:%02d:%02d", remainingHours, remainingMinutes);
     }
 
     public RsData updateAttractionTypeCode(int attractiveTypeCode) {
